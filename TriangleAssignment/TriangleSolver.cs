75ea08a5bbde36f2a20c66b1613a4c7f6345362a
webil <webil@MSI>
2021-07-01 20:25:37 +0530

    Added Triangle Solver class

--- TriangleAssignment/TriangleSolver.cs
+++ TriangleAssignment/TriangleSolver.cs
@@ -1,1 +1,35 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleAssignment
+{
+    public static class TriangleSolver
+    {
+        static string result;
+
+        public static string analyze(int Side_1, int Side_2, int Side_3)
+        {
+            if ((Side_1 + Side_2 > Side_3) && (Side_2 + Side_3 > Side_1) && (Side_1 + Side_3 > Side_2))
+            {
+                if (Side_1 == Side_2 && Side_2 == Side_3)
+                {
+                    result = "Equilateral";
+                }
+
+                else if (Side_1 == Side_2 || Side_1 == Side_3 || Side_2 == Side_3)
+                {
+                    result = "Isosceles";
+                }
+                else
+                {
+                    result = "Scalene";
+                }
+            }
+            return result;
+        }
+
+    }
+
+}
